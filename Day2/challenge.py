473a3503a1a591034c7e34027f50e7fe84e31c9c
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-23 01:12:38 +0530

    Create challenge.py

---
+++ Day2/challenge.py
@@ -0,0 +1,16 @@
+print("Getting to know you!!")
+
+
+name = input("What's your name: ")
+food = input("What's your favourite food: ")
+music = input("What's your favourite music: ")
+location = input("Where do you live:")
+
+print("You are")
+print(name)
+print("You're probably hungry for")
+print(food)
+print("& you're definitely getting your groove on to")
+print(music)
+print("living in the amazing")
+print(location)
