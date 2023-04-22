fd54f8c624329fa727d19e5e5ea591a8d7a14c7e
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-22 17:31:47 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,11 @@
+print("""Harshit
+April 21st 2023
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding 
+along, for a minimum of 10 minutes a dat.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone whenever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling 🥳🥳
+You can follow my progress at replit.com/@harshitmalang""")
