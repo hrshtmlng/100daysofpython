6cf5c8e4294588c350226b191706f397a39b09d6
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-23 16:33:50 +0530

    Create Challenge1.py

---
+++ Day3/Challenge1.py
@@ -0,0 +1,8 @@
+food = input("Name a food > ")
+plant = input("Name a type of plant > ")
+cooking = input("Name a method of cooking > ")
+burnedFood = input("What word describes burned food? >")
+diy = input("Name a DIY item > ")
+
+print("Menu")
+print(cooking ,food, "with", burnedFood ,plant , "on the bed of", diy)
