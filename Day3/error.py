fd1b923fe2101f5789cbc56a4f8da8b49b09a471
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-23 16:23:51 +0530

    Create error.py

---
+++ Day3/error.py
@@ -0,0 +1,8 @@
+# code with error
+# yourName = input("Name: ")
+# whatYear = input("What year is it?: ")
+# print(yourName "thinks it is"  whatYear)
+# solutioncode
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is" , whatYear)
