5ef447471752091255f9d7963217eed1f157c06d
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-23 16:34:49 +0530

    Create Challenge2.py

---
+++ Day3/Challenge2.py
@@ -0,0 +1,12 @@
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing
+song, totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called" name)
+print("Who did something cool like", thing, "at the wonderful", place "where you'll find me", rhyme)
