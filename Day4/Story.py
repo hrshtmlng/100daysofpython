c9a3207a2f69024f5f3e54a17eeefcc44fcf049b
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-24 18:15:07 +0530

    Create Story.py

---
+++ Day4/Story.py
@@ -0,0 +1,17 @@
+print("""Welcome to your adventure
+simulator. I am going to ask you a
+bunch of questions & then create 
+an epic story with you as the star!!""")
+
+name = input("What is your name? ")
+enemy = input("What is your enemy's name? ")
+superpower = input("What is your superpower? ")
+live = input("Where do you live? ")
+food = input("What is your favourite food? ")
+
+print("Hello, ", name ,"""!!! Your ability to 
+""",superpower ,"""will make sure you
+never have to look at""", enemy ,"""again.
+Goo eat""", food ,"""as you walk down the
+streets of""",live , """& use""", superpower ,
+"for good & not evil!!")
