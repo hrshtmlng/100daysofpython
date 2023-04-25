382f79b175452e22e2faf23ae4652d9da9efa4a1
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-26 01:15:31 +0530

    Create main(if-else).py

---
+++ Day5/main(if-else).py
@@ -0,0 +1,6 @@
+myName = input("What's your name?:  ")
+if myName == "Harshit":
+  print("Welcome, Dude!!")
+  print("You're the hairest dude. I've ever seen")
+else:
+  print("Who on the earth are are you?!")
