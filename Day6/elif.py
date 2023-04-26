43909a0fc97b63860d63d47f5c37555468024c65
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-27 01:49:44 +0530

    Create elif.py

---
+++ Day6/elif.py
@@ -0,0 +1,8 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!!")
+elif username == "suzanne":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
