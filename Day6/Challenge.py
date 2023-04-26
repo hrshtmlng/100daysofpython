f9259ab2e9c1facf759745cacd7c996fc43d8b84
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-27 02:05:41 +0530

    Create Challenge.py

---
+++ Day6/Challenge.py
@@ -0,0 +1,12 @@
+print("Secure Login")
+username = input("What is your username?")
+password = input("What is your password?")
+
+if username == "David" and password == "PyThOnR*CKs":
+  print("Welcome, David! You are looking nice today!")
+elif username == "Becky" and password == "Rep!t3eveR":
+  print("Hi Becky! Love your hair today!!")
+elif username == "Booaa" and password == "SmashTheB*gs":
+  print("Yoo! Booaa!! What up?!")
+else:
+  print("You do not have access. Go away")
