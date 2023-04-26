5b67e28b1149550b17ba12ac583c6bbb4e9d8ddb
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-27 01:52:48 +0530

    Create main.py

---
+++ Day6/main.py
@@ -0,0 +1,9 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password >  ")
+if username == "mark" and password == "password":
+  print("Welcome Mark!!")
+elif username == "suzanne" and password == "suzz43nn":
+  print("Hey there Suzanne!")
+else:
+  print("Go away!")
