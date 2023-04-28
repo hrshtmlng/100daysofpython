a1018ff33322996a7f5ae23d043c3f668a4f3ea1
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-28 17:38:22 +0530

    Create Challenge.py

---
+++ Day7/Challenge.py
@@ -0,0 +1,15 @@
+print("Are you a superfan of 'The Big Bang Theory' or a fake fan")
+print()
+print("Answer these questions to find out.")
+
+Glasses = input("Does someone wear glasses?")
+if Glasses == "Yes":
+  print("Correct")
+  WhoGlasses = input("And who wears glasses?")
+  if WhoGlasses == "Leonard":
+    print("You got it")
+  else:
+    print("Try again!!")
+else:
+  print("Wrong!!")
+  
