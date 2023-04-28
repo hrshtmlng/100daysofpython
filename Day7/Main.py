ac4a34864367b8eedd2515bbf5691835f6f6e30c
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-28 14:49:32 +0530

    Create Main.py

---
+++ Day7/Main.py
@@ -0,0 +1,12 @@
+tvShow = input("What is your favorite tv show? ")
+if tvShow == "peppa pig":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "daddy pig":
+    print("Right answer")
+  else:
+    print("Nah, Daddy Pig's the greatest")
+elif tvShow == "paw patrol":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
