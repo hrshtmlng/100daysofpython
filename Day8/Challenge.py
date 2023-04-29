d1f0db12c5088c40f42ec8abc9a52abefd79c74f
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-29 22:48:49 +0530

    Create Challenge.py

---
+++ Day8/Challenge.py
@@ -0,0 +1,32 @@
+
+print("Hello. Welcome to your daily affirmation generator.")
+name = input("What is your name?")
+
+if name == "Harshit" or name == "harshit":
+  DOW = input("What is the day of the week?")
+  if DOW == "monday" or DOW == "monday":
+    print("It is going to be a great Monday", name)
+  if DOW == "Tuesday"or "tuesday":
+    print("What a wonderful Thuesday it is", name)
+  if DOW == "Wednesday" or "wednesday":
+    print("Happy Hump Day", name)
+  if DOW == "Thursday" or DOW == "thursday":
+    print(name,"your week is almost over")
+  if DOW == "Friday" or DOW == "friday":
+    print(name,"It's Friday!")
+elif name == "Peter" or name=="peter":
+  DOW = input("What is the day of the week?")
+  if DOW == "monday" or DOW == "Monday":
+     print("It is going to be a great Monday", name)
+  if DOW == "tuesday" or DOW == "Tuesday":
+    print("You look great in that color", name)
+  if DOW == "wednesday" or DOW == "Wednesday":
+    print("You look chipper today", name) 
+  if DOW == "thursday" or DOW == "Thursday":
+    print(name,"you are doing a great job!")
+  if DOW == "friday" or DOW == "Friday":
+    print(name, "it's FRIDAY!")
+else:
+  print("I do not know your name, but I hope you are having a great day!")
+  
+
