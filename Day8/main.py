98965e3ece7c27fb871f9b483a16d57c8026e884
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-04-29 22:25:44 +0530

    Create main.py

---
+++ Day8/main.py
@@ -0,0 +1,11 @@
+print("Wholesome Positivity Machine")
+print()
+
+
+name = input("Who are you? : ")
+
+goel = input("What do you want to achieve? : ")
+
+scale = input("On a scale of 1-10 how do you feel today?(1: 😥, 10:🥳)<<3 ")
+
+print("Hey", name,", keep your chin up! Today you're goint to", goel,"! in the most amazing way, simple by being you - YOU ROCK!!")
