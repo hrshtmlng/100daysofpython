3c0a0d8994f525885a7a62d0429a5ce2b1eb0728
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-02 21:35:40 +0530

    Create main.py

---
+++ Day10/main.py
@@ -0,0 +1,25 @@
+adding = 4+4
+print(adding)
+
+subtraction = 6-3
+print(subtraction)
+
+multiplication = 3*53
+print(multiplication)
+
+division = 50/4
+print(division)
+
+squared = 4**3
+print(squared)
+
+modulo = 16/3
+print(modulo)
+
+divisor = 17//3
+print(divisor)
+
+print(3.4*6.8)
+print(2467/4673)
+
+print(343//100)
