b20052418e2e5c92ea9e8d0f1ddca6e587524c42
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-06 22:15:09 +0530

    Create RPS.py
    Added rock paper scissors game

---
+++ Day14/RPS.py
@@ -0,0 +1,33 @@
+from getpass import getpass as input
+
+print("E P I C    🪨  📄 ✂️    B A T T L E")
+
+print()
+
+print("Select your move (R, P or S)")
+
+print()
+
+player1Move = input("Player 1 > ")
+player2Move = input("Player 2 > ")
+
+print()
+
+if player1Move == "R":
+  if player2Move == "R":
+    print("You both picked Rock, draw!!")
+  elif player2Move == "S":
+    print("Player1 smashed Player2's Scissors into dust with their Rock!!")
+  else:
+    print("Invaild Move Player2!!")
+elif player1Move == "S":
+  if player2Move == "R":
+    print("Player 2's Rock makes metal-dust out of Player1's Scissors")
+  elif player2Move == "S":
+    print("Ka-Shing! Scissors bounce off each other like a dodgy sword fight!! Draw...")
+  elif player2Move == "P":
+    print("Player1's Scissors make confetti out of Player2's paper!")
+  else:
+    print("Invaild Move Player2!")
+else:
+  print("Invaild Move Player1!")
