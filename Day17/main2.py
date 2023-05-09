50d332c4ccd138f45454233dc08895580d44d3fc
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-09 19:58:21 +0530

    Create main2.py

---
+++ Day17/main2.py
@@ -0,0 +1,10 @@
+while True:
+  print("You are in a corridor, do you go left or right?")
+  direction = input("> ")
+  if direction == "left":
+    print("You have fallen to your death")
+    break
+  elif direction == "right":
+    continue
+  else:
+    print("Ahh! You're a genius, you've won")
