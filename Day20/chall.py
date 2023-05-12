f16303f301934b56368086332ef9ff6303c5736e
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-12 22:24:03 +0530

    Create chall.py
    challenge of day 20

---
+++ Day20/chall.py
@@ -0,0 +1,7 @@
+print("Example:  ")
+start = int(input("Start at: "))
+end = int(input("End before: "))
+increment = int(input("Increment between values: "))
+
+for i in range (start, end, increment):
+  print(i)
