13602b71fe02274e92f905bcd629ad1b26de9c02
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-18 11:24:11 +0530

    Create main.py

---
+++ Day23/main.py
@@ -0,0 +1,13 @@
+def  printmy_Name():
+  print("My Name is David")
+
+# printmy_Name()
+def countToFive():
+  for i in range(1, 6):
+    print(i)
+
+# countToFive()
+def printfavoritecolor():
+  print("My favorite color is red.")
+
+  printfavoritecolor()
