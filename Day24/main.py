c94d178b5ef5891510250ac0e77e4d337691a161
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-31 17:15:50 +0530

    Create main.py

---
+++ Day24/main.py
@@ -0,0 +1,8 @@
+def whichCake(ingredient):
+  if ingredient == "chocolate":
+    print("Mmm, chocolate cake is amazing")
+  elif ingredient == "broccoli":
+    print("You what mate?!!")
+  else:
+    print("Yeah, that's great I suppose....")
+whichCake("broccoli")
