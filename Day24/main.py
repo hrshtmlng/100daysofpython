bfc5a8adf5bc4b686d6c4594c387118e4babcb0d
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-05-31 17:17:50 +0530

    Update main.py

--- Day24/main.py
+++ Day24/main.py
@@ -1,8 +1,10 @@
-def whichCake(ingredient):
+def whichCake(ingredient, base, coating):
   if ingredient == "chocolate":
     print("Mmm, chocolate cake is amazing")
   elif ingredient == "broccoli":
-    print("You what mate?!!")
-  else:
-    print("Yeah, that's great I suppose....")
-whichCake("broccoli")
+    print("You what mate?!")
+  else: 
+    print("Yeah, that's great I suppose...")
+  print("So you want a", ingredient, "cake on a", base, "base with", coating, "on top?")
+
+whichCake("carrot", "biscuit", "icing")
