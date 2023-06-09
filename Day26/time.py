3b6fc3444f96ef69ca5f60844915fc456892f6c6
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-06-10 02:44:21 +0530

    Create time.py

---
+++ Day26/time.py
@@ -0,0 +1,11 @@
+# In this i learned abt importing Time module 
+import os, time
+
+print("Welcome")
+print("To")
+print("Github")
+
+time.sleep(2)
+os.system("clear")
+
+userName = input("Username: ")
