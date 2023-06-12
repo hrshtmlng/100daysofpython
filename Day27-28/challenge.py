9cbf370a6ebac6e8d5b241792417c8e070cfe194
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-06-12 22:12:52 +0530

    Create challenge.py
    Part1

---
+++ Day27-28/challenge.py
@@ -0,0 +1,27 @@
+import random,os, time 
+
+def rollDice(side):
+  result = random.randint(1,side)
+  return result
+
+def health():
+  healthStat = ((rollDice(6)*rollDice(12))/2)+10
+  return strengthStat
+
+while  True:
+  print("⚔️ CHARACTER BUILDER ⚔️")
+  print()
+  name = input("Name your Legend:\n")
+  type = input("Character Type (Human, Elf, Wizard, Orc):\n")
+  print()
+  print(name)
+  print("HEALTH:", health())
+  print("STRENGTH:", strength())
+  print()
+  print("May your name go down in Legend...")
+  print()
+  again = input("Again?:\n")
+  if again == "No" or again =="no":
+    break
+  time.sleep(1)
+  os.system("clear")
