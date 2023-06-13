935d5c6cb3381afd7f20ff0bc541e93f9c7fc15e
Harshit Malang <101478591+hrshtmlng@users.noreply.github.com>
2023-06-13 21:33:13 +0530

    Update challenge.py

--- Day27-28/challenge.py
+++ Day27-28/challenge.py
@@ -3,7 +3,6 @@ import random,os, time
 def rollDice(side):
   result = random.randint(1,side)
   return result
-
 def health():
   healthStat = ((rollDice(6)*rollDice(12))/2)+10
   return strengthStat
